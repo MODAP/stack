3851799f337ad10e30dfdff8dfc5c36c475f8553
Houjun Liu <hliu@shabang.cf>
2021-10-25 16:09:35 -0700

    added optional fidelity things for better estimation

--- brain/src/lib.rs
+++ brain/src/lib.rs
@@ -16,7 +16,11 @@ pub struct Locale {
     // Timestamp of the previous update
     timestamp: SystemTime,
     // Aprogee detection armed
-    armed: bool
+    armed: bool,
+    // Acceleration Cache
+    accel_cache: Vec<na::SVector<f32,3>>,
+    // Fidelity (ms)
+    fidelity: u16
 }
 
 // Implements the locale struct with facilities
@@ -36,7 +40,7 @@ impl Locale {
     /// # Examples
     ///
     /// ```
-    /// let mut loc = brain::Locale::new((0.0,0.0,0.0));
+    /// let mut loc = brain::Locale::new((0.0,0.0,0.0), 500);
     /// loc.update((0.0, 0.3, 0.4)).unwrap(); // DANGEROUS we unwrap b/c it returns a Result
     ///                                       // but it could error with SystemTimeError
     ///                                       // when Rust cannot access the hardware clock
@@ -44,36 +48,54 @@ impl Locale {
     /// println!("{}", loc.position); 
     /// ```
     pub fn update(&mut self, accel:(f32,f32,f32)) -> Result<bool, SystemTimeError>{
-        let elapsed:f32 = self.timestamp.elapsed()?.as_secs_f32();
+        // Push the current stamp into cache
+        self.accel_cache.push(na::Vector3::<f32>::new(accel.0, accel.1, accel.2));
+        self.accel = self.accel_cache.iter().sum();
 
-        self.accel = na::Vector3::<f32>::new(accel.0, accel.1, accel.2);
-        self.velocity += self.accel.scale(elapsed);
-        self.position += self.velocity.scale(elapsed);
+        // Calculate elapsed time since last stamp
+        let elapsed:u16 = (self.timestamp.elapsed()?.as_millis()) as u16;
 
-        self.timestamp = SystemTime::now();
+        // If the temporal fidelity is reached, we integrate
+        if elapsed > self.fidelity {
+            // Reset the caches and timestamps
+            self.timestamp = SystemTime::now();
+            self.accel_cache = vec![];
+
+            // Push the velocity and position values
+            self.velocity += self.accel.scale(elapsed.into());
+            self.position += self.velocity.scale(elapsed.into());
+        }
 
         // Arm the apogee detection of velocity becomes > 5m/s
-        // Arm the apogee detection of velocity becomes > 5m/s
-        return Ok( if self.velocity.magnitude() > 5.0 { self.armed = true; false } 
-                   else if self.armed && self.velocity.magnitude() < 0.1 { true }
+        if self.velocity.magnitude() > 5.0 { self.armed = true; }
+
+        // Check if velocity is smaller than 0.1, then in which case
+        // increment
+        return Ok( if self.armed && self.velocity.magnitude() < 0.1 { true }
                    else { false } )
     }
 
-    /// Initialize the Locale with an initial position
+    /// Initialize the Locale with an initial position and a fidelity
+    ///
+    /// # Aurgements
+    /// - `initial_pos:(f32,f32,f32)`: the initial position of the object
+    /// - `fidelity:u16`: the fidelity in ms to update, larger the # larger the cache
     ///
     /// # Examples
     ///
     /// ```
-    /// let loc = brain::Locale::new((0.0,0.0,0.0));
+    /// let loc = brain::Locale::new((0.0,0.0,0.0), 500);
     /// println!("{}", loc.accel);
     /// ```
-    pub fn new(initial_pos:(f32,f32,f32)) -> Self {
+    pub fn new(initial_pos:(f32,f32,f32), fidelity:u16) -> Self {
         return Locale {
             accel: na::Vector3::<f32>::zeros(),
             velocity: na::Vector3::<f32>::zeros(),
             position: na::Vector3::<f32>::new(initial_pos.0, initial_pos.1, initial_pos.2),
             timestamp: SystemTime::now(),
-            armed: false
+            armed: false,
+            accel_cache: vec![],
+            fidelity
         }
     }
 }
@@ -85,7 +107,7 @@ mod tests {
 
     #[test]
     fn locale() {
-        let mut loc = Locale::new((0.0,40.0,0.0));
+        let mut loc = Locale::new((0.0,40.0,0.0), 500);
 
         // DANGEROUS we unwrap b/c it returns a Result
         // but it could error with SystemTimeError
