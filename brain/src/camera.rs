91fe9e873e1628dabe5eb65381db6158caa669bf
jemoka <hliu@shabang.cf>
2022-07-10 20:56:39 -0700

    Inserted pylon into CI pipeline
    co-authored-by: quantumish <freifeld.david@gmail.com>

--- brain/src/camera.rs
+++ brain/src/camera.rs
@@ -1,1 +1,44 @@
+// Import pylon
+use pylon_cxx;
+
+// Streaming utilites
+#[allow(unused_imports)]
+use tokio_stream::StreamExt;
+
+/// Represents an camera instance
+/// This would abstract away Pylon APIs
+pub struct Camera<'a> {
+    // and the camera
+    pub camera: Option<pylon_cxx::InstantCamera<'a>>
+}
+
+// We will provide initalization and
+// grab facilities
+impl<'a> Camera<'a> {
+
+    /// Initialize camera, try getting a camera, and seeding it
+    pub fn new(pylon:&'a pylon_cxx::Pylon) -> Self {
+        // and create new camera object
+        let cam = pylon_cxx::TlFactory::instance(pylon).create_first_device();
+
+        // Return the camera
+        Camera {
+            camera: match cam {
+                Ok(c) => Some(c),
+                Err(_) => None
+            }
+        }
+    }
+
+    //// SYNCRONOUS OPTS ////
+    // fn camera_info() -> (design and return serialized struct)
+    // fn camera_grab(n) -> Result<ndarray::Array3> # grab n frames
+    // Example: https://github.com/strawlab/pylon-cxx/blob/09ce34be4a84dd63a7a2a0f588ab546412b3bc83/examples/grab.rs#L34-L54
+
+    //// ASYNC OPTS ////
+    // fn camera_stream() -> Result<tokio_stream::Stream<ndarray::Array3>> # streaming buffer
+    // Example: https://github.com/strawlab/pylon-cxx/blob/main/examples/async-grab.rs
+
+    // REMINDER TO WRITE TESTS IN tests.rs
+}
 
---
+++ brain/src/tests.rs
@@ -0,0 +1,38 @@
+// import pylon API to seed pylon for camea
+use pylon_cxx;
+
+// import everything from our end
+use super::*;
+
+//// Unit Tests ////
+
+#[test]
+/// A test for locale creation
+fn locale_create() {
+    let mut _loc = Locale::new((0.0,4000.0,0.0), 500);
+}
+
+#[test]
+/// A test for locale updating
+fn locale_update() {
+    let mut loc = Locale::new((0.0,4000.0,0.0), 500);
+
+    // DANGEROUS we unwrap b/c it returns a Result
+    // but it could error with SystemTimeError
+    // when Rust cannot access the hardware clock
+    // and that case should be handled with `match`
+    // instead of just wrapping
+
+    loc.update((1.0, -9.8, 0.0),
+               (0.0, 0.3, 0.5)).unwrap();
+}
+
+#[test]
+/// A test for pylon camera initalization
+fn camera_initalization() {
+    // create new instance
+    let pylon = pylon_cxx::Pylon::new();
+    // And then create camera
+    let _cam = Camera::new(&pylon);
+}
+
